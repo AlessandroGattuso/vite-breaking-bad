5c7cfac37a6419dda7f840c66e29e0d720a28b1a
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-01-14 15:01:00 +0100

    added load feature

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,47 @@
 <script>
   import AppHeader from './components/AppHeader.vue';
   import AppBody from './components/AppBody.vue';
+  import AppLoader from './components/AppLoader.vue';
   import axios from 'axios'
   import { store } from './store.js'
 
   export default {
     components:{
       AppHeader,
-      AppBody
+      AppBody,
+      AppLoader
     },
     created(){
       this.getCardCollection()
     },
     data(){
       return{
-        store
+        store,
+        loaded: false
       }
     },
     methods:{
       async getCardCollection(){
         await axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Alien')
         .then((answer)=>{
-          store.cardsCollection = (answer.data.data).slice()
-          store.filtered = store.cardsCollection.splice()
+          store.filtered = store.cardsCollection = (answer.data.data).slice()
         });
-        console.log(store.cardsCollection[0])
+        setTimeout(()=>{
+          this.loaded = true
+        }, 1000)
       }
     }
 }
 </script>
 
 <template>
-  <AppHeader></AppHeader>
-  <AppBody></AppBody>
+  <div v-if="loaded == false">
+    <AppLoader></AppLoader>
+  </div>
+  <div v-else>
+    <AppHeader></AppHeader>
+    <AppBody></AppBody>
+  </div>
 </template>
 
 <style lang="sass">
---
+++ src/components/AppLoader.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'AppLoader'
+}
+</script>
+<template>
+  <div class="d-flex justify-content-center align-items-center flex-column">
+    <p>loading</p>
+    <font-awesome-icon class="animation" icon="fa-solid fa-circle-notch" />
+  </div>
+</template>
+<style lang="sass" scoped>
+
+  div
+    height: 100vh
+    width: 100vw
+    p
+      color: #a9a9a9
+      font-size: 10px
+      font-weight: 600
+  .animation
+    font-size: 50px
+    animation: loop 1s linear infinite
+
+
+  @keyframes loop
+    from
+      transform: rotate(0deg)
+    
+    to
+      transform:  rotate(360deg)
+  
+</style>
